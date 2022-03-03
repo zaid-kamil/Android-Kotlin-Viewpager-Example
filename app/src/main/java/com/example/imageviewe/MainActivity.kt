978b29bf5195dba11063f6439f69d31440bb53a0
zaid kamil <xaidmetamorphos@gmail.com>
2022-03-03 15:32:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imageviewe/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.imageviewe
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.imageviewe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+//        binding.content.pager.adapter=myFragmentAdapter
+    }
+
+//    class MyFragmentAdapter : FragmentStatePagerAdapter(){
+//
+//    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+}
