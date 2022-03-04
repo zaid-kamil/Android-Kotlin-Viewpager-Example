d0ff8028dd55fa003d90c21c3675bb31c40cff87
zaid kamil <xaidmetamorphos@gmail.com>
2022-03-04 15:32:07 +0530

    viewpager 100%

--- app/src/main/java/com/example/imageviewe/MainActivity.kt
+++ app/src/main/java/com/example/imageviewe/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.example.imageviewe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +24,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
-//        binding.content.pager.adapter=myFragmentAdapter
+        val adapter = MyPagerAdapter(supportFragmentManager)
+        binding.content.tabs.setupWithViewPager(binding.content.pager)
+        binding.content.pager.adapter = adapter
+        binding.content.pager.setPageTransformer(true,ZoomOutPageTransformer())
+        binding.content.pager.currentItem = 1 // this is optional
     }
 
-//    class MyFragmentAdapter : FragmentStatePagerAdapter(){
-//
-//    }
+    class MyPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        override fun getCount(): Int {
+            return 3
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> OneFragment()
+                1 -> TwoFragment()
+                else -> ThreeFragment()
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when (position){
+                0 -> "One"
+                1 -> "Two"
+                else -> "Three"
+            }
+        }
+
+
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -41,5 +67,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+
+    class ZoomOutPageTransformer : ViewPager.PageTransformer {
+        private val MIN_SCALE = 0.85f
+        private val MIN_ALPHA = 0.5f
+        override fun transformPage(view: View, position: Float) {
+            view.apply {
+                val pageWidth = width
+                val pageHeight = height
+                when {
+                    position < -1 -> { // [-Infinity,-1)
+                        // This page is way off-screen to the left.
+                        alpha = 0f
+                    }
+                    position <= 1 -> { // [-1,1]
+                        // Modify the default slide transition to shrink the page as well
+                        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                        val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                        val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                        translationX = if (position < 0) {
+                            horzMargin - vertMargin / 2
+                        } else {
+                            horzMargin + vertMargin / 2
+                        }
+
+                        // Scale the page down (between MIN_SCALE and 1)
+                        scaleX = scaleFactor
+                        scaleY = scaleFactor
+
+                        // Fade the page relative to its size.
+                        alpha = (MIN_ALPHA +
+                                (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                    }
+                    else -> { // (1,+Infinity]
+                        // This page is way off-screen to the right.
+                        alpha = 0f
+                    }
+                }
+            }
+        }
+    }
 
 }
